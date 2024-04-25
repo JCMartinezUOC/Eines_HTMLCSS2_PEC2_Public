1524b3e4f36300803fbb9889a1a99ab5a25f5289
JCMartinezUOC <jchicam@uoc.edu>
2024-04-25 21:27:01 +0200

    Dynamic header & footer

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -11,7 +11,45 @@ import * as bootstrap from 'bootstrap';
  * Write any other JavaScript below
  */
 
+if (document.title) {
+  switch (document.title) {
+    case "Chefs de pueblo: Portada":
+      // Header
+      document.querySelector('#nav-header').classList.add("navbar--poster-bg");
+      document.querySelector('#navbar-header-toggler').classList.add("navbar--poster");
+      document.querySelector('#navbar-header-toggler-icon').classList.add("navbar--poster","navbar--poster-toggler");
+      document.querySelector('#nav-link-header-2').classList.add("navbar--poster");
+      document.querySelector('#nav-link-header-3').classList.add("navbar--poster");
+      document.querySelector('#nav-link-header-4').classList.add("navbar--poster");
+      // Footer
+      document.querySelector('#nav-link-footer-1').classList.add("active");
+      break;
+    case "Chefs de pueblo: Los chefs":
+      // Header
+      document.querySelector('#nav-link-header-2').classList.add("active");
+      // Footer
+      document.querySelector('#nav-link-footer-2').classList.add("active");
+      break;
+    case "Chefs de pueblo: El pueblo":
+      // Header
+      document.querySelector('#nav-link-header-3').classList.add("active");
+      // Footer
+      document.querySelector('#nav-link-footer-3').classList.add("active");
+      break;
+    case "Chefs de pueblo: Galeria":
+      // Header
+      document.querySelector('#nav-link-header-4').classList.add("active");
+      // Footer
+      document.querySelector('#nav-link-footer-4').classList.add("active");
+      break;
+    default:
+      break;
+  }
+}
+
+/*
 +( function() {
   const university = "UOC";
   console.log(`Hello, ${university}!`);
 } )();
+*/
